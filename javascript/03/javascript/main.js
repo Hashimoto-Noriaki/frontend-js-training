35a1c033beb4a63d45cc57bd2ced2e409786ed46
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-12 23:02:35 +0900

    first commit

---
+++ javascript/03/javascript/main.js
@@ -0,0 +1,30 @@
+const number = 10;
+{
+  const score = 150;
+  console.log(number);
+  console.log(score);
+}
+
+const programmingLanguage = 'Javascript';
+console.log(`言語は${programmingLanguage}です。`);
+
+// console.log(number);
+// console.log(score);
+
+// const a = 1;
+
+// {
+//   const b = 2;
+//   console.log(a);
+//   console.log(b);
+// }
+
+// console.log(a);
+// console.log(b);
+
+// {
+//   var global = 'グローバル';
+//   let local = 'ローカル';
+// }
+// console.log(global);
+// console.log(local);
