71fa0ad3aca291ac93a357e29582edbffd451381
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-21 23:24:54 +0900

    アラートとテキスト

--- javascript/04/javascript/main.js
+++ javascript/04/javascript/main.js
@@ -1,38 +1,38 @@
-// function plus1(number) {
-//   const result = number + 1;
-//   return result;
-// }
-
-// // const number = 20;
-// // const result = plus1(number);
-// // console.log(result);
-
-// //引数のない関数の定義
-// function showMessage() {
-//   const message = 'text';
-//   alert(message);
-// }
+function plus1(number) {
+  const result = number + 1;
+  return result;
+}
 
 // const number = 20;
 // const result = plus1(number);
 // console.log(result);
 
-// showMessage;
-// showMessage();
+//引数のない関数の定義
+function showMessage() {
+  const message = 'text';
+  alert(message);
+}
+
+const number = 20;
+const result = plus1(number);
+console.log(result);
+
+showMessage;
+showMessage();
 
 
-// function threetimes(fn) {
-//   //3回関数をする処理
-//   fn(); //引数の処理を実行
-//   fn();
-//   fn();
-// }
+function threetimes(fn) {
+  //3回関数をする処理
+  fn(); //引数の処理を実行
+  fn();
+  fn();
+}
 
-// const callfunction = function () {
-//   console.log('コールバック関数');
-// };
+const callfunction = function () {
+  console.log('コールバック関数');
+};
 
-// threetimes(callfunction);
+threetimes(callfunction);
 
 function doTwice(fn) {
   fn();
