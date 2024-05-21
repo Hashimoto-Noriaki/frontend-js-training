772abc22a74a4822618357a0fb972952530fba8d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-21 23:00:52 +0900

    関数の練習

--- javascript/04/javascript/main.js
+++ javascript/04/javascript/main.js
@@ -1,35 +1,47 @@
-function plus1(number) {
-  const result = number + 1;
-  return result;
-}
+// function plus1(number) {
+//   const result = number + 1;
+//   return result;
+// }
+
+// // const number = 20;
+// // const result = plus1(number);
+// // console.log(result);
+
+// //引数のない関数の定義
+// function showMessage() {
+//   const message = 'text';
+//   alert(message);
+// }
 
 // const number = 20;
 // const result = plus1(number);
 // console.log(result);
 
-//引数のない関数の定義
-function showMessage() {
-  const message = 'text';
-  alert(message);
-}
-
-const number = 20;
-const result = plus1(number);
-console.log(result);
-
-showMessage;
-showMessage();
+// showMessage;
+// showMessage();
 
 
-function threetimes(fn) {
-  //3回関数をする処理
-  fn(); //引数の処理を実行
+// function threetimes(fn) {
+//   //3回関数をする処理
+//   fn(); //引数の処理を実行
+//   fn();
+//   fn();
+// }
+
+// const callfunction = function () {
+//   console.log('コールバック関数');
+// };
+
+// threetimes(callfunction);
+
+function doTwice(fn) {
   fn();
   fn();
 }
 
-const callfunction = function () {
-  console.log('コールバック関数');
+const sayHello = function () {
+  console.log('Hello');
+  return 'Hello';
 };
 
-threetimes(callfunction);
+doTwice(sayHello);
