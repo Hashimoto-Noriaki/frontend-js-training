167159b979b17c1ba822f1972f1aaf3acc2acbdc
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-06 20:20:25 +0900

    論理演算子の条件分岐

--- javascript/07/javascript/main.js
+++ javascript/07/javascript/main.js
@@ -1,18 +1,33 @@
-const number = 10;
+// const number = 10;
 
-if (number > 10) {
-    console.log('10以上');
-} else if (number === 10) {
-    console.log('10');
-} else {
-    console.log('10未満');
-}
+// if (number > 10) {
+//     console.log('10以上');
+// } else if (number === 10) {
+//     console.log('10');
+// } else {
+//     console.log('10未満');
+// }
 
-const val = 10;
-if (val) {
-  console.log('真');
-} else {
-  console.log('偽');
-}
+// const val = 10;
+// if (val) {
+//   console.log('真');
+// } else {
+//   console.log('偽');
+// }
 //10
 //0なら偽
+
+const age = 100;
+const hometown = '千葉';
+
+// '埼玉かつ100以上'を出力する条件：年齢が100以上かつ出身地が埼玉
+if (age <= 100 && hometown === '埼玉') {
+  console.log('埼玉かつ100以上');
+}
+
+// '埼玉または100以上'を出力する条件：年齢が100以上または出身地が埼玉
+if (age >= 100 || hometown === '千葉') {
+  console.log('千葉または100以上');
+}
+//千葉または100以上
+
