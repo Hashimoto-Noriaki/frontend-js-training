f8f2b82b53c60f11f578cb3c51a40f293d9f210b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-06 20:22:18 +0900

    コメントアウト削除

--- javascript/07/javascript/main.js
+++ javascript/07/javascript/main.js
@@ -1,19 +1,19 @@
-// const number = 10;
+const number = 10;
 
-// if (number > 10) {
-//     console.log('10以上');
-// } else if (number === 10) {
-//     console.log('10');
-// } else {
-//     console.log('10未満');
-// }
+if (number > 10) {
+    console.log('10以上');
+} else if (number === 10) {
+    console.log('10');
+} else {
+    console.log('10未満');
+}
 
-// const val = 10;
-// if (val) {
-//   console.log('真');
-// } else {
-//   console.log('偽');
-// }
+const val = 10;
+if (val) {
+  console.log('真');
+} else {
+  console.log('偽');
+}
 //10
 //0なら偽
 
