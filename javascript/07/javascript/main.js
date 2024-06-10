b3ce85ad3cc1bfc549f5102dcbac5ba584421e56
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-11 02:17:12 +0900

    三項演算子

--- javascript/07/javascript/main.js
+++ javascript/07/javascript/main.js
@@ -1,19 +1,19 @@
-const number = 10;
+// const number = 10;
 
-if (number > 10) {
-    console.log('10以上');
-} else if (number === 10) {
-    console.log('10');
-} else {
-    console.log('10未満');
-}
+// if (number > 10) {
+//     console.log('10以上');
+// } else if (number === 10) {
+//     console.log('10');
+// } else {
+//     console.log('10未満');
+// }
 
-const val = 10;
-if (val) {
-  console.log('真');
-} else {
-  console.log('偽');
-}
+// const val = 10;
+// if (val) {
+//   console.log('真');
+// } else {
+//   console.log('偽');
+// }
 //10
 //0なら偽
 
@@ -41,3 +41,16 @@ if (val) {
 //   console.log('外国人');
 // }
 //30以下または日本人
+
+
+//三項演算子
+const planet = true;
+const people = planet ? '地球人' : '宇宙人';
+console.log(people);
+
+const isLate = false;
+const result = isLate ? 'はよ寝ろ' : 'ええやん';
+console.log(result);
+
+//地球人
+//ええやん
