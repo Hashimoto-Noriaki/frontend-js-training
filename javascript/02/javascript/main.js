7fe6a6a93bb29965b1ee13832ff824ef4c35f5b7
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-03 19:32:56 +0900

    関数で実行

--- javascript/02/javascript/main.js
+++ javascript/02/javascript/main.js
@@ -6,3 +6,10 @@ const user = {
 };
 
 console.log(user);
+
+function execute({ animal1, animal2, animal3 }) {
+    return { animal1, animal2, animal3 };
+}
+    const result = execute({ animal1: '犬', animal2: '猫', animal3: '猿' });
+    console.log(result);
+  //{animal1: '犬', animal2: '猫', animal3: '猿'}
